5074f772258b6aa3da23ed16cd4cbcb1fa5837c4
Ubuntu <ubuntu@ip-172-31-26-48.us-east-2.compute.internal>
2019-11-04 03:37:46 +0000

    created file with code from registration tutorial

---
+++ register.js
@@ -0,0 +1,31 @@
+$(function(){
+    $("#register").on('click', function(event){
+        event.preventDefault();
+        var username   = $("#username").val();
+        var password   = $("#password").val();
+        
+        if(!username || !password ||){ 
+            $("#msgDiv").show().html("All fields are required.");
+        }
+        else{ 
+            $.ajax({
+                url: "/register",
+                method: "POST",
+                data: { user_name: username, password: password }
+            }).done(function( data ) {
+                if ( data ) {
+                    if(data.status == 'error'){
+                        var errors = '<ul>';
+                        $.each( data.message, function( key, value ) {
+                            errors = errors +'<li>'+value.msg+'</li>';
+                        });
+                        errors = errors+ '</ul>';
+                        $("#msgDiv").html(errors).show();
+                    }else{
+                        $("#msgDiv").removeClass('invalid').addClass('valid').html(data.message).show(); 
+                    }
+                }
+            });
+        }
+    });
+});
